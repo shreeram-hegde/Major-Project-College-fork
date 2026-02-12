70ae409fd18fd74f35330e14fe228b0567a94e71
shreeram-hegde <shreeeramhegde@gmail.com>
2026-02-12 22:27:34 +0530

    fixed conflicts

--- backend/index.js
+++ backend/index.js
@@ -13,13 +13,10 @@ const port = 5001;
 const connectDB = require("./config/db.js");
 const authRoutes = require("./routes/authRoute.js");
 const mlRoutes = require('./routes/MLRoute');
-<<<<<<< HEAD
 const rescueRoutes = require('./routes/rescue');
-=======
 const uploadRoute = require('./routes/uploadRoute'); 
 const floodRoute = require('./routes/floodRoute');
 
->>>>>>> 9897e742c824732e2b730438a4b4b3454245b8c7
 //connect to db:
 connectDB();
 
@@ -37,9 +34,7 @@ app.use(
 app.use("/api/auth", authRoutes);
 //ML route:
 app.use('/api/flood', mlRoutes);
-<<<<<<< HEAD
 app.use('/api/rescue', rescueRoutes);
-=======
 app.use('/api/upload', uploadRoute); 
 // Static serving of ML data images
 const mlDataDir = path.join(__dirname, '..', 'ML', 'ml-api', 'data');
@@ -47,7 +42,6 @@ app.use('/data', express.static(mlDataDir));
 
 // Flood routes
 app.use('/api/flood', floodRoute);
->>>>>>> 9897e742c824732e2b730438a4b4b3454245b8c7
 
 
 app.listen(port, () => {
